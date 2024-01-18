d8a291186e36e318d850a631dcf5887639116fad
Anyknown <156967900+Anyknown@users.noreply.github.com>
2024-01-17 21:50:33 -0500

    Adding hello world script

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2580
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .NewYork-Regular;}
+{\colortbl;\red255\green255\blue255;\red14\green47\blue29;\red249\green240\blue229;}
+{\*\expandedcolortbl;;\cssrgb\c5098\c23529\c14902;\cssrgb\c98039\c95294\c91765;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs44 \cf2 \cb3 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 print("Hello World!")\
+}
